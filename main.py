7f73f9d45814a604f2c18caacc29c70ecc8e6311
yanisimova <yanisimova@magnite.com>
2022-03-29 21:46:07 +0300

    send request with headers

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ r = requests.get('https://google.com')
 print(r.status_code)
 print(r.headers)
 
+
+def is_status_code_equals(url, status_code):
+    r = requests.get(url)
+    return r.status_code == status_code
+
+
+print(is_status_code_equals('https://google.com', 200))
